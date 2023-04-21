0d272c223fee02a6e883ddd1c7b086a74f9d181f
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2023-04-21 13:07:48 -0400

    Initial Commit

---
+++ awards.js
@@ -0,0 +1,64 @@
+module.exports = [
+  {
+    year: '2019-2020',
+    text: 'ACG Houston Deal of the Year Finalist',
+    img: '/images/awards/2020ACG.webp',
+    link: 'https://www.gap-advisors.com/news/gap-closing-chosen-as-an-acg-houston-deal-of-the-year-finalist',
+  },
+  {
+    year: 'October 2020',
+    text: 'Axial Top 20 Lower Middle Market Investment Bank',
+    img: '/images/awards/Axial.webp',
+    link: 'https://www.gap-advisors.com/news/gap-advisors-has-been-named-a-top-20-lower-middle-market-investment-bank-by-axial',
+  },
+  {
+    year: 'January 2021',
+    text: 'Houston Business Journal Recognizes GaP in M&A Deal',
+    img: '/images/awards/Houston.webp',
+    link: 'https://www.gap-advisors.com/news/houston-business-journal-hbj-recognizes-gap-advisors-in-m-a-deal',
+  },
+  {
+    year: '2021',
+    text: 'Acquisition International recognizes GaP as Most Trusted Lower Mid-Market M&A Advisor',
+    img: '/images/awards/Acquisition.webp',
+    link: 'https://www.gap-advisors.com/news/acquisition-international-ai-has-recognized-gap-as-the-most-trusted-lower-mid-market-m-a-advisors-usa-for-2021',
+  },
+  {
+    year: '2021',
+    text: 'ACG Houston Deal of the Year Finalist',
+    img: '/images/awards/2021ACG.webp',
+    link: 'https://www.gap-advisors.com/news/once-again-a-gap-facilitated-transaction-chosen-as-an-acg-houston-deal-of-the-year-finalist',
+  },
+  {
+    year: 'March 2022',
+    text: 'ACG Houston Deal of the Year Finalist',
+    img: '/images/awards/2022ACG.webp',
+    link: 'https://www.gap-advisors.com/news/a-gap-facilitated-transaction-once-again-chosen-as-an-acg-houston-deal-of-the-year-finalist',
+  },
+  {
+    year: '2022',
+    text: 'BBB Torch Award for Ethics Runner-Up',
+    img: '/images/awards/2022BBB.webp',
+    link: 'https://www.gap-advisors.com/news/gilbert-pardue-runner-up-in-2022-bbb-torch-award-for-ethics',
+  },
+  {
+    year: '2022',
+    text: "Inc. 5000's fastest growing companies",
+    img: '/images/awards/inc500FastestGrowing.webp',
+    link: 'https://www.gap-advisors.com/news/gilbert-pardue-transaction-advisors-amongst-inc-5000s-fastest-growing-companies-for-2022',
+  },
+  // add new awards below this line
+  {
+    year: '2022',
+    text: "GaP 2022 Best Business Transaction Advisory Firm in Texas",
+    img: '/images/awards/2022/bestBusinessTransaction.webp',
+    link: 'https://www.gap-advisors.com/news/acquisition-international-ai-has-recognized-gap-as-winner-of-the-m-a-awards-2022-best-business-transaction-advisory-firm-in-texas',
+  },
+  {
+    year: '2023',
+    text: "GaP 30 Top Innovative Brands of 2023",
+    img: '/images/awards/2023/topInnovativebrands.webp',
+    link: 'https://www.gap-advisors.com/news/gilbert-and-pardue-transaction-advisors-an-award-winning-lower-middle-market-m-a-advisory-firm',
+  },
+  // add new awards above this line
+]
